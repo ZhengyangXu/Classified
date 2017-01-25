c33220ad01e83fd2c8ef1a7603f03535cab63318
Seed Zeng <cdzengpeiyun@gmail.com>
2017-01-25 09:57:54 -0500

    three LCA

---
+++ Binary_Tree & Divide and Conquer/LCA/3.19_474_Lowest_common_AncestorII.py
@@ -0,0 +1,39 @@
+"""
+Description
+______________
+Tree contians both node
+have parent link
+"""
+
+
+"""
+Definition of ParentTreeNode:
+class ParentTreeNode:
+    def __init__(self, val):
+        self.val = val
+        self.parent, self.left, self.right = None, None, None
+"""
+
+
+class Solution:
+    """
+    @param root: The root of the tree
+    @param A and B: Two node in the tree
+    @return: The lowest common ancestor of A and B
+    """
+
+    def lowestCommonAncestorII(self, root, A, B):
+        # Write your code here
+        A_visited = []
+        B_visited = []
+
+        while A is not None or B is not None:
+            A_visited.append(A)
+            B_visited.append(B)
+            if A in B_visited:
+                return A
+            if B in A_visited:
+                return B
+            A = A.parent
+            B = B.parent
+        return None
