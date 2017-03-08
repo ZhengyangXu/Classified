2a241f237912b8d43fe703f4ed084c16567b42bd
Seed Zeng <cdzengpeiyun@gmail.com>
2017-03-08 16:12:46 -0500

    adding and renaming

---
+++ 2.Binary_Search/2.14_459_Closest_Number_In_Sorted_Array.py
@@ -0,0 +1,39 @@
+"""
+Description
+__________
+Given a target number and an integer array
+A sorted in ascending order, find the index i in A
+such that A[i] is closest to the given target.
+Return -1 if there is no element in the array.
+
+Example
+________
+Given [1, 2, 3] and target = 2, return 1.
+Given [1, 4, 6] and target = 3, return 1.
+Given [1, 4, 6] and target = 5, return 1 or 2.
+Given [1, 3, 3, 4] and target = 2, return 0 or 1 or 2.
+"""
+
+
+class Solution:
+    # @param {int[]} A an integer array sorted in ascending order
+    # @param {int} target an integer
+    # @return {int} an integer
+
+    def closestNumber(self, A, target):
+        # Write your code here
+        start, end = 0, len(A) - 1
+        while start + 1 < end:
+            mid = start + (end - start) / 2
+            v = A[mid]
+            if v == target:
+                return mid
+            elif v < target:
+                start = mid
+            else:
+                end = mid
+
+        if abs(A[start] - target) < abs(A[end] - target):
+            return start
+        else:
+            return end
---
+++ 2.Binary_Search/2.15_159_Find_Minimum_in_Rotated_Sorted_Array.py
@@ -0,0 +1,46 @@
+"""
+Description
+___________
+Given a mountain sequence of
+n integers which increase firstly and then decrease
+find the mountain top.
+Have you met this question in a real interview? Yes
+
+Example
+__________
+Given nums = [1, 2, 4, 8, 6, 3] return 8
+Given nums = [10, 9, 8, 7], return 10
+
+Approach
+_________
+This translates to find the first element that has
+nums[index] > nums[index-1]
+
+Complexity
+__________
+Lg(N)
+"""
+
+
+class Solution:
+    # @param {int[]} nums a mountain sequence which increase firstly and then decrease
+    # @return {int} then mountain top
+
+    def mountainSequence(self, nums):
+        # Write your code here
+        if nums is None or len(nums) == 0:
+            return
+        if len(nums) == 1:
+            return nums[0]
+        start, end = 0, len(nums) - 1
+
+        while start + 1 < end:
+            mid = start + (end - start) / 2
+            if nums[mid] >= nums[mid + 1]:
+                end = mid
+            else:
+                start = mid
+        if nums[start] > nums[start + 1]:
+            return nums[start]
+        else:
+            return nums[end]
